51167b6ba92bf94530f56b2f266d03aa42c4d322
jps5418 <johnphillip.sims@mavs.uta.edu>
2025-10-04 15:53:31 -0500

    added some hopefully useful classes to helpers.py in utils 2

--- app/utils/helpers.py
+++ app/utils/helpers.py
@@ -5,12 +5,18 @@
 
 
 #class representing an entry for a past trade in a user's trade history
+#   stockName is a string
+#   stockPrice is a number
+#   sharesTraded is a number
+#   totalCost is a number
+#   timeMade is the time and date at which the trade was carried out
 class pastTrade:
-    def __init__ (self, stockName, stockPrice, sharesTraded, totalCost)
+    def __init__ (self, stockName, stockPrice, sharesTraded, totalCost, timeMade):
         self.stockName = stockName
         self.stockPrice = stockPrice
         self.sharesTraded = sharesTraded
         self.totalCost = totalCost
+        self.timeMade = timeMade
 
 #a class representing a stock that a user owns
 class stockEntry:
@@ -18,8 +24,12 @@ class stockEntry:
         self.stockName = stockName
         self.amountOwned = amountOwned
 
-
 #a class containing data about a user
+#   username is a string
+#   currencyOwned is an number (probably a float)
+#   stocksOwned is a list of stockEntry objects
+#   savedArticles is a list of links
+#   tradeHistory is a list of pastTrade objects sorted by chronological order
 class userData:
     def __init__(self, username, currencyOwned, stocksOwned, stocksFollowed, savedArticles, tradeHistory):
         self.username = username
