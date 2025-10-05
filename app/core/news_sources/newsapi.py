634b577a286eb715fcb503aa5b786ae4553a429a
jps5418 <johnphillip.sims@mavs.uta.edu>
2025-10-04 19:37:00 -0500

    figured out how google_news_api works

--- app/core/news_sources/newsapi.py
+++ app/core/news_sources/newsapi.py
@@ -1,3 +1,19 @@
+from google_news_api import GoogleNewsClient
+
 class NewsAPI:
     def fetch_news(self, ticker: str):
-        return [f"{ticker} hits record high!", f"{ticker} faces market challenges"]
+        client = GoogleNewsClient(
+            language="en",
+            country="US",
+            requests_per_minute=60,
+            cache_ttl=300
+        )
+
+        articles = client.search(f"{ticker} stocks", when="7d", max_results = 3)
+
+        for article in articles:
+            return [f"{article['title']}"]
+        #for topic in news.items:
+
+        #    return article.title
+        #return [f"{ticker} hits record high!", f"{ticker} faces market challenges"]
